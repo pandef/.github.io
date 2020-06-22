96d8217e7a97ef4c44a2b331248f738cd5832315
pandef <67216348+pandef@users.noreply.github.com>
2020-06-22 14:49:40 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+$(function(){
+
+$('h1').fadeIn(2500);
+
+
+$('.kami6').hover(
+  function(){
+    $('.kami7').fadeIn();
+  },
+  function(){
+    $('.kami7').fadeOut();
+  }
+);
+
+
+$('.kami8').hover(
+  function(){
+    $('.kami9').fadeIn();
+  },
+  function(){
+    $('.kami9').fadeOut();
+  }
+);
+
+
+});
